a35c4f87c33d86a7a8e389c7aff0a56d1f6d9a5a
LaythAlqadhi <125077045+LaythAlqadhi@users.noreply.github.com>
2024-02-20 01:53:05 +0000

    Add introduction component

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Introduction() {
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center">
+      <span className="text-4xl font-bold">Layth Alqadhi</span>
+      <span className="text-lg text-neutral-400 font-bold">Full-stack web developer</span>
+    </div>
+  );
+}
+
+export default Introduction;
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Introduction from '../components/Introduction';
 
 function Index() {
   return (
     <div className="bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-100">
+      <Introduction />
     </div>
   );
 }
