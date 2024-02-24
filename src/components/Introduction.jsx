474c6e15301a723678fa7cee4402f40d33eac675
Layth <125077045+LaythAlqadhi@users.noreply.github.com>
2024-02-24 03:32:25 +0000

    Update Introduction component.
    - Add opacity effect
    - Add scroll down icon

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function Introduction() {
+  const elementsRef = useRef([]);
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      elementsRef.current.forEach((el) => {
+        /* eslint-disable-next-line no-param-reassign */
+        el.style.opacity = 1;
+      });
+    }, 150);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <div className="h-screen w-full flex flex-col justify-center items-center">
-      <span className="text-4xl text-yellow-400 font-bold">Layth Alqadhi</span>
-      <span className="text-lg text-neutral-400 font-bold">Full-stack web developer</span>
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <span
+        className="text-primary text-4xl text-yellow-600 opacity-0 transition-opacity duration-1000"
+        ref={(e) => elementsRef.current.push(e)}
+      >
+        LAYTH ALQADHI
+      </span>
+      <span
+        className="text-secondary text-lg opacity-0 transition-opacity delay-700 duration-1000"
+        ref={(e) => elementsRef.current.push(e)}
+      >
+        Full-stack web developer
+      </span>
+      <span
+        className="material-symbols-outlined text-secondary animate-bounce text-4xl opacity-0 transition-opacity delay-[1400ms] duration-1000"
+        aria-label="Scroll down"
+        ref={(e) => elementsRef.current.push(e)}
+      >
+        arrow_drop_down
+      </span>
     </div>
   );
 }
