000f727c1253793767f252627aec4612dace86e4
Layth <125077045+LaythAlqadhi@users.noreply.github.com>
2024-02-24 03:43:26 +0000

    Add Contact component

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { MessageProvider } from './contexts/MessageContext';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <MessageProvider>
+        <App />
+      </MessageProvider>
     </Router>
   </React.StrictMode>,
 );
